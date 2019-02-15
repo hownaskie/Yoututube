3691bdf42a51c1994afb2c88a02327390b769791
Jonas Colipapa <jonascolipapa@gmail.com>
2019-02-15 10:18:31 +0800

    Initial Commit

---
+++ Yoututube/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Yoututube
+//
+//  Created by JonasC on 15/02/2019.
+//  Copyright © 2019 JonasC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Yoututube/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Yoututube
+//
+//  Created by JonasC on 15/02/2019.
+//  Copyright © 2019 JonasC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
